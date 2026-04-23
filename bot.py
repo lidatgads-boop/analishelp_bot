bcb3ac5116ca9ce09d30994a48958f8eb3cefc6a
lidatgads-boop <lidatgads@gmail.com>
2026-04-23 16:40:15 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from handlers import common, education, quiz, faq
+
+logging.basicConfig(level=logging.INFO)
+
+BOT_TOKEN = "8714921910:AAG8UxngcorHbxkbVDX9kzHLG6QwLaBI6Bg"
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.include_router(common.router)
+    dp.include_router(education.router)
+    dp.include_router(quiz.router)
+    dp.include_router(faq.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
